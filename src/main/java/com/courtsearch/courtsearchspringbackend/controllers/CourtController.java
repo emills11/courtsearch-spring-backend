36891ec3f4997a561ae8a5595158b41003d83604
emills11 <ratdog45@gmail.com>
2021-06-16 15:56:01 -0400

    Added cross origin from frontend

--- src/main/java/com/courtsearch/courtsearchspringbackend/controllers/CourtController.java
+++ src/main/java/com/courtsearch/courtsearchspringbackend/controllers/CourtController.java
@@ -6,6 +6,7 @@ import com.courtsearch.courtsearchspringbackend.model.CourtModel;
 import com.courtsearch.courtsearchspringbackend.repository.CourtRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class CourtController {
         this.courtRepository = courtRepository;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/api/courts")
     public List<CourtModel> getCourts() {
         return courtRepository.findAll();
--- src/main/java/com/courtsearch/courtsearchspringbackend/controllers/KeyController.java
+++ src/main/java/com/courtsearch/courtsearchspringbackend/controllers/KeyController.java
@@ -6,6 +6,7 @@ import com.courtsearch.courtsearchspringbackend.model.KeyModel;
 import com.courtsearch.courtsearchspringbackend.repository.KeyRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class KeyController {
         this.keyRepository = keyRepository;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/api/keys")
     public List<KeyModel> getKey(@RequestParam String type) {
         return keyRepository.findByKeyType(type);
