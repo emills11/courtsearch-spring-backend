885c404a7af606c7b5c812078cbaae47fe203b83
emills11 <ratdog45@gmail.com>
2021-07-01 14:32:30 -0400

    Wrapped CourtController and KeyController methods as ResponseEntity

--- src/main/java/com/courtsearch/courtsearchspringbackend/controllers/CourtController.java
+++ src/main/java/com/courtsearch/courtsearchspringbackend/controllers/CourtController.java
@@ -1,11 +1,14 @@
 package com.courtsearch.courtsearchspringbackend.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.courtsearch.courtsearchspringbackend.model.CourtModel;
 import com.courtsearch.courtsearchspringbackend.repository.CourtRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +25,19 @@ public class CourtController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/api/courts")
-    public List<CourtModel> getCourts() {
-        return courtRepository.findAll();
+    public ResponseEntity<List<CourtModel>> getCourts() {
+        try {
+            List<CourtModel> courts = new ArrayList<CourtModel>();
+
+            courtRepository.findAll().forEach(courts::add);
+
+            if (courts.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(courts, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/courtsearch/courtsearchspringbackend/controllers/KeyController.java
+++ src/main/java/com/courtsearch/courtsearchspringbackend/controllers/KeyController.java
@@ -1,11 +1,14 @@
 package com.courtsearch.courtsearchspringbackend.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.courtsearch.courtsearchspringbackend.model.KeyModel;
 import com.courtsearch.courtsearchspringbackend.repository.KeyRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +26,19 @@ public class KeyController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/api/keys")
-    public List<KeyModel> getKey(@RequestParam String type) {
-        return keyRepository.findByKeyType(type);
+    public ResponseEntity<List<KeyModel>> getKey(@RequestParam String type) {
+        try {
+            List<KeyModel> keys = new ArrayList<KeyModel>();
+
+            keyRepository.findByKeyType(type).forEach(keys::add);
+
+            if (keys.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(keys, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
