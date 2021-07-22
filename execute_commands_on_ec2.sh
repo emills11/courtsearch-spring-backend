338c0adcc722776cf9444ccc673de532d799d915
emills11 <ratdog45@gmail.com>
2021-07-22 17:08:41 -0400

    Implemented HTTPS

--- execute_commands_on_ec2.sh
+++ execute_commands_on_ec2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-lsof -ti tcp:8080 | xargs kill
-echo "Killed process running on port 8080"
+PORT_NUMBER=8443
+lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
+echo "Killed process running on port 8443"
 
 nohup java -jar courtsearch-spring-backend-0.0.1-SNAPSHOT.jar &
 echo "Started server using java -jar command"
